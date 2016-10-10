a4d6d542b5b91e69b3a613e07da111e3e7a1db3b
Fernando Lemos <fcl2@cesar.org.br>
2016-10-10 19:07:49 -0300

    adicionando npm

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+// SET UP - get all the tools
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
 app.use(express.static('public'));
 // make express look in the public directory for custom assets (css/js/img)
